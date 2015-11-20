9031b8ea4053d9c4ea3d9a074d63ede4ca38eae7
Nikita Khmourovitch <dev.nkh@gmail.com>
2015-11-20 10:45:42 +0300

    carry language stats

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var request = require('request');
 var _ = require('lodash');
-var apiUrl = '/api/2/';
-var host = 'https://transifex.com';
+var apiUrl = 'https://transifex.com/api/2/';
 var Promise = require('bluebird');
 var utils = require('./lib/utils');
 var generateHash = utils.generateHash;
 var mergeStrings = utils.mergeStrings;
 var applyTagsToStrings = utils.applyTagsToStrings;
+
 /**
  *
  * @param {{login:String, password:String, projectSlug: String, resourceSlug: String, skipTags: Array[String], obsoleteTag:String, requestConcurrency: Number, stringWillRemove:{tags:Array[String]}}} config
@@ -20,9 +20,8 @@ var transifex = function (config) {
 
     var makeRequest = function (url, method, data) {
         method = method || 'GET';
-        // console.log(method + ' ', apiUrl + url);
         var options = {
-            url: `${host}${apiUrl}${url}`,
+            url: `${apiUrl}${url}`,
             method: method,
             auth: {
                 'user': config.login,
@@ -47,25 +46,53 @@ var transifex = function (config) {
     var getResponse = function (url) {
         return makeRequest(url);
     };
-    var getLanguages = function () {
-        var url = `project/${config.projectSlug}/?details`;
+
+    var getProjectLanguageCodes = function (projectSlug) {
+        var url = `project/${projectSlug}/languages`;
         return getResponse(url).then(function (data) {
-            return data.teams;
+            return _.pluck(data, 'language_code');
         });
     };
+
+    var getProjectLanguageDetails = function (projectSlug, languageCode) {
+        return getResponse(`project/${projectSlug}/language/${languageCode}?details`);
+    };
+
     var getTranslation = function (langCode) {
         var url = `project/${resourceFile}translation/${langCode}/?mode=reviewed`;
         return getResponse(url).then(function (data) {
-            langCode = langCode.replace('_', '-');
-            return {lang: langCode, content: data.content};
+            return data.content;
         });
     };
+
     var getTranslatedResources = function () {
-        return getLanguages().then(function (languages) {
-            var resources = languages.map(getTranslation);
-            return Promise.all(resources)
-        })
+        var projectSlug = config.projectSlug;
+        return getProjectLanguageCodes(projectSlug)
+            .then(function (languageCodes) {
+                return Promise.all(languageCodes.map(function (languageCode) {
+                    return Promise
+                        .all([
+                            getProjectLanguageDetails(projectSlug, languageCode),
+                            getTranslation(languageCode)
+                        ])
+                        .then(function (results) {
+                            var projectLanguageDetails = results[0];
+                            var translation = results[1];
+                            return {
+                                lang: languageCode.replace('_', '-'),
+                                content: translation,
+                                stats: {
+                                    totalTokensCount: projectLanguageDetails['total_segments'],
+                                    translatedTokensCount: projectLanguageDetails['translated_segments'],
+                                    reviewedTokensCount: projectLanguageDetails['reviewed_segments'],
+                                    translatedWordsCount: projectLanguageDetails['translated_words']
+                                }
+                            };
+                        })
+                }));
+            })
     };
+
     var getResourceStrings = function (strings) {
         return Promise.map(_.toArray(strings), function (token) {
             var url = `project/${resourceFile}source/${generateHash(token)}`;
@@ -75,6 +102,7 @@ var transifex = function (config) {
             });
         }, {concurrency: concurrency});
     };
+
     var putResourceStrings = function (strings) {
         return Promise.map(strings, function (value) {
             var url = `project/${resourceFile}source/${generateHash(value.token)}`;
@@ -83,12 +111,15 @@ var transifex = function (config) {
             return strings;
         });
     };
+
     var getLanguagesInfo = function () {
         var url = `languages/`;
         return getResponse(url).then(function (languages) {
             return languages.map(function (lang) {
-                lang.code = lang.code.replace('_', '-');
-                return lang;
+                return {
+                    code: lang.code.replace('_', '-'),
+                    name: lang.name
+                };
             });
         })
     };
@@ -98,7 +129,6 @@ var transifex = function (config) {
         var url = `project/${resourceFile}content/`;
         return makeRequest(url, 'PUT', {content: JSON.stringify(content)})
     };
-
 
     var updateResourceFile = function (dictionaries) {
         var url = `project/${resourceFile}content/`;
@@ -110,7 +140,7 @@ var transifex = function (config) {
         }).then(function (res) {
             return Promise.all([getResourceStrings(res[1].updateStrings), res[1].obsoleteStrings]);
         }).then(function (res) {
-            return applyTagsToStrings(dictionaries,res[0], res[1], config)
+            return applyTagsToStrings(dictionaries, res[0], res[1], config)
         }).then(function (strings) {
             return putResourceStrings(strings);
         }).then(function (strings) {
